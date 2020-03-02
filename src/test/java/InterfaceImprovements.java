afe35b5c4c52652f4321b2ce83b8b643268b0804
souljungkim <souljungkim@gmail.com>
2020-03-03 02:59:37 +0900

    Update InterfaceImprovements

--- src/test/java/InterfaceImprovements.java
+++ src/test/java/InterfaceImprovements.java
@@ -6,15 +6,29 @@ public class InterfaceImprovements {
     /**************************************************
      * Ready
      **************************************************/
-    interface TestInterface {
+    interface TestInterface1 {
         Integer testSomeMethod();
-        static String testStaticMethod(String param1){ return param1; }
-        default int testDefaultMethod(int a, int b){ return a + b; }
+        Long testSameNameMethod();
+        static String testStaticMethod(String param1){ return param1 +"1"; }
+        default int testDefaultMethod1(int a, int b){ return a + b; }
+//        default int testSameNameDefaultMethod(int a, int b){ return a + b; }
     }
 
-    class TestClass implements TestInterface {
+    interface TestInterface2 {
+        Integer testSomeMethod();
+        Long testSameNameMethod();
+        static String testStaticMethod(String param1){ return param1 +"2"; }
+        default int testDefaultMethod2(int a, int b){ return a * b; }
+//        default int testSameNameDefaultMethod(int a, int b){ return a + b; }
+    }
+
+    class TestClass implements TestInterface1, TestInterface2  {
         @Override
         public Integer testSomeMethod() { return 12345; }
+        @Override
+        public Long testSameNameMethod(){ return 5292L; }
+        //Cannot implements if both default method name are same
+//        default int testSameNameDefaultMethod(int a, int b){ return a + b; }
     }
 
 
@@ -25,15 +39,20 @@ public class InterfaceImprovements {
     @Test
     public void testInterfaceImprovements(){
         //(New) Static method in interface
-        assert TestInterface.testStaticMethod("Hello?").equals("Hello?");
+        assert TestInterface1.testStaticMethod("Hello?").equals("Hello?1");
+        assert TestInterface2.testStaticMethod("Hello?").equals("Hello?2");
 
         TestClass tc = new TestClass();
 
         //(New) Default method in interface
-        assert tc.testDefaultMethod(1, 2) == 3;
+        assert tc.testDefaultMethod1(1, 2) == 3;
+        assert tc.testDefaultMethod2(1, 2) == 2;
+        // Cannot use same name default method between multiple interfaces
+//        assert tc.testSameNameDefaultMethod(1, 2) == 3;
 
         //Implemented method
         assert tc.testSomeMethod() == 12345;
+        assert tc.testSameNameMethod() == 5292L;
     }
 
 
